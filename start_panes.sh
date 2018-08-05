99dfc0d393033fd026d82acc40da57f26a44809d
Frankie Liu <frankie.y.liu@gmail.com>
2018-08-05 08:48:14 -0700

    Adding nkn

--- start_panes.sh
+++ start_panes.sh
@@ -1,6 +1,7 @@
-tmux send-keys -t ":1.0" 'python blockchain.py' C-m
+kedge=$(pwd)
+tmux send-keys -t ":1.0" "cd ${kedge}/blockchain; python blockchain.py" C-m
 sleep 0.4
-tmux send-keys -t ":1.1" 'python blockchain.py' C-m
+tmux send-keys -t ":1.1" "cd ${kedge}/blockchain; python blockchain.py" C-m
 sleep 0.4
-tmux send-keys -t ":1.2" 'python blockchain.py' C-m
-tmux send-keys -t ":1.3" 'python blockchain_client.py' C-m
+tmux send-keys -t ":1.2" "cd ${kedge}/blockchain; python blockchain.py" C-m
+tmux send-keys -t ":1.3" "cd ${kedge}/blockchain-client; python blockchain_client.py" C-m
